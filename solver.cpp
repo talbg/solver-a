f70c70948ee6e9945a43190f4d2fedb0d82e362a
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-25 21:07:58 +0300

    Add files via upload

--- solver.cpp
+++ solver.cpp
@@ -1,2 +1,33 @@
-dg
-sd
+
+class solver {
+	RealVariable x;
+    ComplexVariable y;
+	
+	double solve(e){
+		
+	}
+	
+}
+
+class RealVariable {
+	private:
+		double a, b, c;
+	public:
+		RealVariable(): a(0), b(0), c(0){}
+		RealVariable(double x, double y, double z): a(x), b(y), c(z){}
+		
+		double operator() (double x) { 
+			return _a*x*x + _b*x + _c; }
+
+		double operator()() { 
+			return 0; 
+		}
+
+};
+
+double	solve(RealVariable x);
+std::complex<double>	solve(RealVariable x);
+
+RealVariable operator+ (RealVariable x, double y) { 
+	return _a*x*x + _b*x + _c; 
+}
--- solver.hpp
+++ solver.hpp
@@ -1,2 +1,42 @@
-sdf
-sdf
+
+#include <iostream>
+#include <complex>
+
+namespace solver{
+	class RealVariable {
+	// private:
+		// double a, b, c;
+	// public:
+		// RealVariable(): a(0), b(0), c(0){}
+		// RealVariable(double x, double y, double z): a(x), b(y), c(z){}
+		
+		// double operator() (double x) { 
+			// return _a*x*x + _b*x + _c; }
+
+		// double operator()() { 
+			// return 0; 
+		// }
+
+    };
+	
+	class ComplexVariable {
+		// double a, b;
+		
+	};
+	class Equation {
+		enum  { Real, Complex };
+	public:
+		double a, b, c;
+		
+		Equation operator==(Equation e);
+	}
+}
+
+
+
+Equation operator==(Equation e1, Equation e2);
+Equation operator==(Equation e1, double a);
+Equation operator==(double a, Equation e1);
+
+double	solve(RealVariable x);
+std::complex<double>	solve(ComplexVariable x);
