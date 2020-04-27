d8e1b9554b7647bf33dbc3b8843dccc7c52548a8
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-27 21:28:45 +0300

    Add files via upload

--- solver.hpp
+++ solver.hpp
@@ -7,60 +7,46 @@ namespace solver{
 	class RealVariable {
 	public:
 		double a, b, c;
-	RealVariable(){
-		a=0, b=0, c=0; 
-	}
+		RealVariable()
+		: a(0), b(0), c(0){}
 	
-		friend RealEquation &operator+ (double &e);
-		friend RealEquation &operator- (double &e);
-		friend RealEquation &operator/ (double &e);
-		friend RealEquation &operator^ (double &e);
-		friend RealEquation &operator==(RealEquation &e);
-		friend RealEquation &operator*(double a, RealVariable &e1);
-};
-	
-	class RealEquation {
-	public:
-		double a, b, c;
-	
-
-		
-		RealEquation operator==(RealEquation e);
-		RealEquation operator==(double x);
-		RealEquation operator+(RealEquation e);
-		RealEquation operator+(double x);
-		RealEquation operator/(double x);
-	}
-	RealEquation operator()(RealEquation e);
-	RealEquation operator==(double a, RealEquation e1);
+		friend RealVariable& operator+(double e);
+		friend RealVariable& operator+(RealVariable e);
+		friend RealVariable& operator-(double e);
+		friend RealVariable& operator-(RealVariable e);
+		friend RealVariable& operator/(double e);
+		friend RealVariable& operator^(double e);
+		friend RealVariable& operator==(RealVariable e);
+		friend RealVariable& operator==(double e);
+		// friend RealVariable& operator/(RealVariable e);
+		// friend RealVariable& operator*(RealVariable e1);
+		// friend RealVariable& operator*(double a);
+	};
+	RealVariable& operator()(RealVariable e);
+	RealVariable& operator*(double a, RealVariable e);
+	RealVariable& operator==(double a, RealVariable e);
 	
 	class ComplexVariable {
 	public:
-		ComplexVariable operator+(double e);
-		ComplexVariable operator-(double e);
-		ComplexVariable operator/(double e);
-		ComplexVariable operator^(double e);
-		ComplexVariable operator==(complexEquation e);
-	};
-	RealEquation operator*(double a, RealVariable e1);
-	
-	class complexEquation {
-	public:
-		std::complex<double> a, b, c;
+		double a, b, c;
 		
-		complexEquation operator==(complexEquation e);
-		complexEquation operator==(double x);
-		complexEquation operator+(complexEquation e);
-		complexEquation operator+(double x);
-		complexEquation operator/(double x);
-	}
-	friend complexEquation operator()(complexEquation &e);
-	friend complexEquation operator==(std::complex<double> &a, complexEquation &1);
+		ComplexVariable()
+		: a(0), b(0), c(0){}
+		friend ComplexVariable& operator+(std::complex<double> e);
+		friend ComplexVariable& operator+(ComplexVariable e);
+		friend ComplexVariable& operator-(std::complex<double> e);
+		friend ComplexVariable& operator-(ComplexVariable e);
+		friend ComplexVariable& operator/(double e);
+		friend ComplexVariable& operator^(double e);
+		friend ComplexVariable& operator==(ComplexVariable e);
+		friend ComplexVariable& operator==(std::complex<double>);
+	};
+	ComplexVariable& operator()(ComplexVariable e);
+	ComplexVariable& operator*(double a, ComplexVariable e1);
+	ComplexVariable& operator==(double a, ComplexVariable e);
 
-};
-
-	double solve(RealEquation Real);
-	std::complex<double>	solve(ComplexVariable x);
+	double solve(RealVariable Real);
+	std::complex<double> solve(ComplexVariable x);
 };
 
 
