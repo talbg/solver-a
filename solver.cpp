83723e3687b5f97f38fe114fee64e8b47ab393f5
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-28 00:52:40 +0300

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -4,18 +4,23 @@
 #include <cmath>
 using namespace solver;
 
-
+RealVariable& operator+(solver::RealVariable& x, solver::RealVariable e){
+    return x;
+}
 RealVariable& operator+(solver::RealVariable& x, double e){
     return x;
 }
-RealVariable& operator+(solver::RealVariable& x, solver::RealVariable e){
-    return x;
+RealVariable& operator+(double x, solver::RealVariable& e){
+    return e;
 }
 RealVariable& operator-(solver::RealVariable& x, double e){
     return x;
 }
 RealVariable& operator-(solver::RealVariable& x, solver::RealVariable e){
     return x;
+}
+RealVariable& operator*(double a, solver::RealVariable& e){
+    return e;
 }
 RealVariable& operator/(solver::RealVariable& x, double e){
     return x;
@@ -26,23 +31,27 @@ RealVariable& operator^(solver::RealVariable& x, double e){
 RealVariable& operator==(solver::RealVariable& x, solver::RealVariable e){
     return x;
 }
-RealVariable& operator+(double x, solver::RealVariable& e){
+RealVariable& operator==(double a, solver::RealVariable& e){
     return e;
+}
+
+ComplexVariable& operator+(solver::ComplexVariable& x, solver::ComplexVariable e){
+    return x;
 }
 ComplexVariable& operator+(solver::ComplexVariable& x, std::complex<double> e){
     return x;
 }
+ComplexVariable& operator+(double x, ComplexVariable& e){
+    return e;
+} 
+ComplexVariable& operator-(solver::ComplexVariable& x, solver::ComplexVariable e){
+    return x;
+} 
 ComplexVariable& operator-(solver::ComplexVariable& x, std::complex<double> e){
     return x;
 }
-ComplexVariable& operator+(solver::ComplexVariable& x, solver::ComplexVariable e){
-    return x;
-}
-ComplexVariable& operator+(double x, ComplexVariable& e){
-    return e;
-}  
-ComplexVariable& operator-(solver::ComplexVariable& x, solver::ComplexVariable e){
-    return x;
+ComplexVariable& operator*(double a, solver::ComplexVariable& e1){
+    return e1;
 }
 ComplexVariable& operator/(solver::ComplexVariable& x, double e){
     return x;
@@ -56,18 +65,10 @@ ComplexVariable& operator==(solver::ComplexVariable& x, solver::ComplexVariable 
 ComplexVariable& operator==(solver::ComplexVariable& x , std::complex<double>){
     return x;
 }
-RealVariable& operator*(double a, solver::RealVariable& e){
-    return e;
-}
-RealVariable& operator==(double a, solver::RealVariable& e){
-    return e;
-}
-ComplexVariable& operator*(double a, solver::ComplexVariable& e1){
-    return e1;
-}
 ComplexVariable& operator==(double a, solver::ComplexVariable& e){
     return e;
 }
+
 double solve(solver::RealVariable Real){
     return 0;
 }
