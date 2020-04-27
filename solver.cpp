7dc1edf5411930df6dad83c73e5bebb2b27532cc
talbg <57101649+talbg@users.noreply.github.com>
2020-04-27 22:37:47 +0300

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -50,18 +50,18 @@ solver::ComplexVariable& operator==(this, ComplexVariable e){
 solver::ComplexVariable& operator==(this , std::complex<double>){
     retrun &this;
 }
-solver::RealVariable& operator()(RealVariable& e){
-    return &e;
-}
+// solver::RealVariable& operator()(RealVariable& e){
+//     return &e;
+// }
 solver::RealVariable& operator*(double a, RealVariable& e){
     return &e;
 }
 solver::RealVariable& operator==(double a, RealVariable& e){
     return &e;
 }
-solver::ComplexVariable& operator()(ComplexVariable& e){
-    return &e;
-}
+// solver::ComplexVariable& operator()(ComplexVariable& e){
+//     return &e;
+// }
 solver::ComplexVariable& operator*(double a, ComplexVariable& e1){
     return &e;
 }
